1be2f23fde837222ead386614254097894f9556a
HarshitaGITHB <harshithalaxmanrao@gmail.com>
2021-11-03 19:06:45 -0500

    secound commit

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var palanquinRouter = require('./routes/palanquin');
-
+var palanquinRouter = require('./routes/palanquin');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -23,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/palanquin', palanquinRouter);
+app.use('/addmods', addmodsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
