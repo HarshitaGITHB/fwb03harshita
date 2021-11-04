9ac5729b3cc9a15dc66645f3a27fd8c743760738
HarshitaGITHB <harshithalaxmanrao@gmail.com>
2021-11-03 19:32:54 -0500

    third commit

--- routes/palanquin.js
+++ routes/palanquin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('palanquin', { title: 'Search Results Gas' });
+  res.render('palanquin', { title: 'Search Results Palanquin' });
 });
 
 module.exports = router;
